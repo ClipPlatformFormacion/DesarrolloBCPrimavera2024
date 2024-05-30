2c7dc48358e27a0c795458f501e19bb20e78b032
Cristina Nicolàs <cristina.nicolas@clipplatform.com>
2024-05-30 15:50:32 +0200

    script creación contenedores

---
+++ Documentación/InstalacionContenedorDocker.ps1
@@ -0,0 +1,5 @@
+﻿#Install-Module -Name BcContainerHelper -Force
+
+$artifactURL = Get-BCArtifactUrl -country es -select Latest -type Sandbox
+
+New-BcContainer -accept_eula -auth UserPassword -artifactUrl $artifactURL -containerName BC24CU1ES -useBestContainerOS -updateHosts -includeTestToolkit -includeTestLibrariesOnly
